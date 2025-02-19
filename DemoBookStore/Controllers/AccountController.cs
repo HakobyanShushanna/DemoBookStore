a4b6cf011904859dcaec7af20fcbb4421f11b6d6
HakobyanShushanna <shushanna.hakobyan.rc@gmail.com>
2025-02-19 19:41:25 +0400

    rdtrdtry

--- DemoBookStore/Controllers/AccountController.cs
+++ DemoBookStore/Controllers/AccountController.cs
@@ -1,10 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DemoBookStore.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DemoBookStore.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly SignInManager<UserModel> _signInManager;
 
+        public AccountController(SignInManager<UserModel> signInManager)
+        {
+            _signInManager = signInManager;
+        }
         public IActionResult Login()
         {
             return View();
@@ -13,6 +20,16 @@ namespace DemoBookStore.Controllers
         public IActionResult Register()
         {
             return View();
+        }
+        public IActionResult Logout()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> LogoutConfirmed()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
         }
     }
 }
